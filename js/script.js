81b3142fc38c89ade6aeace71f67fefbe9dc8b72
DanByczynski18 <DByczynski18@gmail.com>
2016-06-15 19:05:20 -0500

    Added infodiv html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $(".tableelement").click(function () {
-
+    $(".infobox").empty();
     $("#mainbox").before('<div class="infobox"><h1>TEST</h1></div>');
-
   });
 });
